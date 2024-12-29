641c435809bf18644f8b76d7349a788c95454f82
jacobmcmichael <jacobmcmichael@gmail.com>
2024-12-29 15:15:42 -0800

    :recycle: extracts list items into react component

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,6 @@
+/* Types */
+import { ListItemType } from "@app/types/components";
+
 /* Styles */
 import "@styles/components/list.css";
 
@@ -5,26 +8,31 @@ const data = [
 	{
 		id: 1,
 		name: "Item 1",
+		checked: false,
 	},
 	{
 		id: 2,
 		name: "Item 2",
+		checked: false,
 	},
 	{
 		id: 3,
 		name: "Item 3",
+		checked: false,
 	},
 	{
 		id: 4,
 		name: "Item 4",
+		checked: false,
 	},
 	{
 		id: 5,
 		name: "Item 5",
+		checked: false,
 	},
 ];
 
-export default function List() {
+function ListItem(item: ListItemType) {
 	const handleCheckboxClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
 		const currentTarget: HTMLDivElement = event.currentTarget;
 		const checkbox: HTMLInputElement | null = currentTarget.querySelector('input[type="checkbox"]');
@@ -35,25 +43,27 @@ export default function List() {
 	};
 
 	return (
+		<div
+			key={item.id}
+			className="form__group"
+			onClick={(event) => handleCheckboxClick(event)}
+		>
+			<label htmlFor={item.name}>{item.name}</label>
+			<input
+				id={item.name}
+				type="checkbox"
+				name={item.name}
+				defaultChecked={item.checked}
+			/>
+		</div>
+	);
+}
+
+export default function List() {
+	return (
 		<section id="List">
 			<div className="inner">
-				<form id="ShoppingListForm">
-					{data.length > 0 &&
-						data.map((item) => (
-							<div
-								key={item.id}
-								className="form__group"
-								onClick={(event) => handleCheckboxClick(event)}
-							>
-								<label htmlFor={item.name}>{item.name}</label>
-								<input
-									id={item.name}
-									type="checkbox"
-									name={item.name}
-								/>
-							</div>
-						))}
-				</form>
+				<form id="ShoppingListForm">{data.length > 0 && data.map((item) => <ListItem {...item} />)}</form>
 			</div>
 		</section>
 	);
--- src/types/components.ts
+++ src/types/components.ts
@@ -1,5 +1,11 @@
 import { ButtonHTMLAttributes } from "react";
 
+export type ListItemType = {
+	id: number;
+	name: string;
+	checked: boolean;
+};
+
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	variant: "primary" | "secondary";
 }
